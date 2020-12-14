2a038c42db353217c696400b62a5cd66886d1e08
Ecanoz <emincanozbey@gmail.com>
2020-12-14 17:01:59 +0300

    Boşluk düzenlemesi yapıldı.

--- upload.php
+++ upload.php
@@ -1,10 +1,10 @@
-<?php
-if (0 < $_FILES['file']['error']) {
-    echo 'Error: ' . $_FILES['file']['error'] . '<br>';
-} else {
-    $date = new DateTime();
-    $mili = $date->getTimestamp();
-    $isim = 'uploads/' . $mili . $_FILES['file']['name'];
-    move_uploaded_file($_FILES['file']['tmp_name'], $isim);
-    echo $isim;
-}
+<?php
+if (0 < $_FILES['file']['error']) {
+    echo 'Error: ' . $_FILES['file']['error'] . '<br>';
+} else {
+    $date = new DateTime();
+    $mili = $date->getTimestamp();
+    $isim = 'uploads/' . $mili . $_FILES['file']['name'];
+    move_uploaded_file($_FILES['file']['tmp_name'], $isim);
+    echo $isim;
+}
